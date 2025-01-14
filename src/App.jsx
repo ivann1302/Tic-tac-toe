0de5e8c597d7c263c7f90f9c72d9aade93b751fc
ivan <ivann97n@gmail.com>
2025-01-14 11:35:08 +0300

    add jumpTo function in Game component;

--- src/App.jsx
+++ src/App.jsx
@@ -79,7 +79,7 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true );
+  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const currentSquares = history[history.length - 1];
 
@@ -88,13 +88,31 @@ export default function Game() {
     setXIsNext(!xIsNext);
   }
 
+  function jumpTo(nextMove) {
+    // TODO
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description ='Go to game start';
+    }
+    return (
+      <li>
+        <button onClick ={()=> jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
+
   return (
     <div className='game'>
       <div className='game-board'>
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
     <div className='game-info'>
-      <ol>{/*TODO*/}</ol>
+      <ol>{moves}</ol>
     </div>
     </div>
   );
