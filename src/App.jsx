38547db642cf9a50d7371d1997017d68bf3b6218
ivan <ivann97n@gmail.com>
2025-01-14 10:37:57 +0300

    start to make Game component

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function Square({ value, onSquareClick }) {
   )
 }
 
-export default function Board() {
+ function Board() {
   const [xIsNext, setXIsNext] = useState(true );
   const [squares, setSquares] = useState(Array(9).fill(null));
 
@@ -21,27 +21,6 @@ export default function Board() {
   } else {
     status = "Next player " + (xIsNext ? 'X' : 'O');
   }
-
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-
 
   function handleClick(i) {
     if(squares[i] || calculateWinner(squares)) {
@@ -79,5 +58,37 @@ export default function Board() {
     </div>
     </>
   )
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 }
 
+export default function Game() {
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board />
+      </div>
+    <div className='game-info'>
+      <ol>{/*TODO*/}</ol>
+    </div>
+    </div>
+  );
+}
