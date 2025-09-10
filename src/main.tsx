a57421fb17f0e239e8a01b913749900bbb99d15c
ivan <ivann97n@gmail.com>
2025-09-10 16:42:38 +0300

    feat: made structure for routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import Game from './components/app/App';
+import App from './components/app/App';
+import { BrowserRouter as Router} from "react-router-dom";
 import { GameProvider } from './context/Game-context';
 import { ThemeProvider } from './context/Theme-context.tsx';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <ThemeProvider>
-      <GameProvider>
-        <h1 className={'title'}>Tic-tac-toe</h1>
-        <Game />
-      </GameProvider>
-    </ThemeProvider>
+      <Router>
+          <ThemeProvider>
+              <GameProvider>
+                  <h1 className={'title'}>Tic-tac-toe</h1>
+                  <App />
+              </GameProvider>
+          </ThemeProvider>
+      </Router>
   </StrictMode>
 );
